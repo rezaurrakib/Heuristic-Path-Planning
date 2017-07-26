00124f6fd3dbb29ea28ed3430e654685d7ccec9b
Till Söhlemann <t.soehlemann@gmail.com>
2017-07-26 18:15:22 +0200

    Add project for simplified car agent

---
+++ SimpleAgent/environment.py
@@ -0,0 +1,80 @@
+import numpy as np
+import random
+
+class Environment:
+
+
+    #start_config: 0 = right; 1 = down; 2 = left; 3 = top
+    def __init__(self, width, height, obstacles, goal_x, goal_y, start_pos_x, start_pos_y, start_config):
+        self.width = width
+        self.height = height
+        self.obstacles = obstacles
+        self.goal_x = goal_x
+        self.goal_y = goal_y
+        self.start_x = start_pos_x
+        self.start_y = start_pos_y
+        self.start_config = start_config
+        self.agent_pos_x = start_pos_x
+        self.agent_pos_y = start_pos_y
+        self.agent_config = start_config
+        self.number_states = width * height * 4
+        self.actions = [-1, 0, 1]
+
+    #-1 = turn left; 0 = move straight; 1 = turn right
+    def step(self, action):
+        reward = 0
+        stop = False
+        #move straight
+        if action == 0:
+            if self.agent_config == 0:
+                self.agent_pos_x += 1
+            elif self.agent_config == 1:
+                self.agent_pos_y -= 1
+            elif self.agent_config == 2:
+                self.agent_pos_x -= 1
+            elif self.agent_config == 3:
+                self.agent_pos_y += 1
+            #check if obstacle
+            for (o_x, o_y) in self.obstacles:
+                if o_x == self.agent_pos_x and o_y == self.agent_pos_y:
+                    reward = -10
+                    stop = True
+                    return np.zeros(self.width * self.height * 4), reward, stop
+            #check if grid is left:
+            if self.agent_pos_x >= self.width or self.agent_pos_x < 0 or self.agent_pos_y >= self.height or self.agent_pos_y < 0:
+                reward = -10
+                stop = True
+                return np.zeros(self.width * self.height * 4), reward, stop
+            #check if goal is reached
+            if self.goal_x == self.agent_pos_x and self.goal_y == self.agent_pos_y:
+                reward = 10
+                stop = True
+        #else: turn, no reward, no stopping
+        else:
+            self.agent_config += action
+            self.agent_config %= 4
+
+        #TODO VISUALIZE ENVIRONMENT
+        #PARAMETERS:
+        #self.agent_config (0=right, 1=top, 2=left, 3=down) -> turning angle
+        #self.width, self.height (grid size)
+        #self.obstacles -> coordinates for obstacles
+        #self.goal_x, self.goal_y -> goal coordinates
+        #self.agent_pos_x, self.agent_pos_y -> agent coordinates
+
+        #print(str(self.width)+"; "+str(self.height)+"; "+str(self.agent_pos_x)+"; "+str(self.agent_pos_y)+"; "+str(self.agent_config))
+        state_one_hot = np.eye(self.width*self.height*4)[(self.agent_pos_y*self.width) + self.agent_pos_x+(self.agent_config*self.height*self.width)]
+        return state_one_hot, reward, stop
+        #return (self.agent_pos_x, self.agent_pos_y, self.agent_config), 0, 0
+
+    def get_random_action(self):
+        return random.choice(self.actions)
+
+    def reset(self):
+        self.agent_pos_x = self.start_x
+        self.agent_pos_y = self.start_y
+        self.agent_config = self.start_config
+        #return (self.agent_pos_x, self.agent_pos_y, self.agent_config)
+        state_one_hot = np.eye(self.width * self.height * 4)[
+            (self.agent_pos_y * self.width) + self.agent_pos_x + (self.agent_config * self.height * self.width)]
+        return state_one_hot
---
+++ SimpleAgent/train_agent.py
@@ -0,0 +1,77 @@
+import gym
+import numpy as np
+import random
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from environment import Environment
+
+environment = Environment(width=10, height=10, obstacles=[(1, 0), (4, 4), (8, 3), (7, 6)],
+                          goal_x=9, goal_y=9, start_pos_x=0, start_pos_y=0, start_config=0)
+
+tf.reset_default_graph()
+
+#These lines establish the feed-forward part of the network used to choose actions
+inputs1 = tf.placeholder(shape=[1,environment.number_states],dtype=tf.float32)
+W = tf.Variable(tf.random_uniform([environment.number_states,3],0,0.01))
+Qout = tf.matmul(inputs1,W)
+predict = tf.argmax(Qout,1)
+
+#Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
+nextQ = tf.placeholder(shape=[1,3],dtype=tf.float32)
+loss = tf.reduce_sum(tf.square(nextQ - Qout))
+trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+updateModel = trainer.minimize(loss)
+
+init = tf.initialize_all_variables()
+
+# Set learning parameters
+y = .99
+e = 0.1
+num_episodes = 2000
+#create lists to contain total rewards and steps per episode
+jList = []
+rList = []
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(num_episodes):
+        if i%50 == 0:
+            print("Starting episode "+str(i))
+        #Reset environment and get first new observation
+        s = environment.reset()
+        rAll = 0
+        d = False
+        j = 0
+        #The Q-Network
+        while j < 99:
+            j+=1
+            #Choose an action by greedily (with e chance of random action) from the Q-network
+            a,allQ = sess.run([predict,Qout],feed_dict={inputs1:[s]})
+            if np.random.rand(1) < e:
+                a[0] = environment.get_random_action()
+            #Get new state and reward from environment
+            s1,r,d = environment.step(a[0])
+            #Obtain the Q' values by feeding the new state through our network
+            Q1 = sess.run(Qout,feed_dict={inputs1:[s1]})
+            #Obtain maxQ' and set our target value for chosen action.
+            maxQ1 = np.max(Q1)
+            targetQ = allQ
+            targetQ[0,a[0]] = r + y*maxQ1
+            #Train our network using target and predicted Q values
+            _,W1 = sess.run([updateModel,W],feed_dict={inputs1:[s],nextQ:targetQ})
+            rAll += r
+            s = s1
+            if d == True:
+                #Reduce chance of random action as we train the model.
+                e = 1./((i/50) + 10)
+                break
+        jList.append(j)
+        rList.append(rAll)
+print("Percent of succesful episodes: " + str(sum(rList)/num_episodes) + "%")
+print(rList)
+print(jList)
+
+plt.plot(rList)
+
+plt.plot(jList)
+
+plt.show()
